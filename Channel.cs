542c654922e4653192fac8f04b9ebb424121be4b
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-06-18 19:55:20 +0300

    Add Channel<T> class functionality

--- Channel.cs
+++ Channel.cs
@@ -1,7 +1,15 @@
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
 namespace rocket_bot;
 
 public class Channel<T> where T : class
 {
+	private List<T> Values = new();
+	readonly object locker = new();
+
 	/// <summary>
 	/// Возвращает элемент по индексу или null, если такого элемента нет.
 	/// При присвоении удаляет все элементы после.
@@ -11,11 +19,42 @@ public class Channel<T> where T : class
 	{
 		get
 		{
-			return null;
-		}
+			lock (locker)
+			{
+                if (index < 0 || index >= this.Values.Count)
+                {
+                    return null;
+                }
+                return this.Values[index];
+            }
+        }
 		set
 		{
-		}
+			lock(locker)
+			{
+                if (index > this.Values.Count || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), "index out of collection range");
+                }
+                else if(index == this.Values.Count)
+                {
+                    var newCollection = new List<T>(index + 1);
+					newCollection.AddRange(this.Values);
+                    newCollection.Add(value);
+                    this.Values = newCollection;
+                }
+				else
+				{
+                    var newCollection = new List<T>(index + 1);
+                    for (int i = 0; i <= index; i++)
+					{
+						newCollection.Add(this.Values[i]);
+					}
+                    newCollection[index] = value;
+                    this.Values = newCollection;
+                }
+            }
+        }
 	}
 
 	/// <summary>
@@ -23,15 +62,25 @@ public class Channel<T> where T : class
 	/// </summary>
 	public T LastItem()
 	{
-		return null;
-	}
+		lock(locker)
+		{
+            return this[this.Values.Count - 1];
+        }
+    }
 
 	/// <summary>
 	/// Добавляет item в конец только если lastItem является последним элементом
 	/// </summary>
 	public void AppendIfLastItemIsUnchanged(T item, T knownLastItem)
 	{
-	}
+		lock(locker)
+		{
+            if (this.LastItem() == knownLastItem)
+            {
+                this[this.Values.Count] = item;
+            }
+        }
+    }
 
 	/// <summary>
 	/// Возвращает количество элементов в коллекции
@@ -40,7 +89,10 @@ public class Channel<T> where T : class
 	{
 		get
 		{
-			return 0;
-		}
+			lock(locker)
+			{
+                return this.Values.Count;
+            }
+        }
 	}
 }
